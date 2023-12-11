2e04cb1e23c58c9c453ec29cf34a467629d85287
nurulfauzi <nurulfauzi347@gmail.com>
2023-12-11 13:35:39 +0700

    fix sidebar mobile

--- src/components/sidebar/toggleButton/ToggleButton.jsx
+++ src/components/sidebar/toggleButton/ToggleButton.jsx
@@ -8,6 +8,7 @@ const ToggleButton = ({ setOpen }) => {
 		strokeWidth="3" 
 		stroke="black" 
 		strokeLinecap="round"
+		d= "M 3 16.5 L 17 2.5"
 		variants={{
 			closed: { d: "M 2 2.5 L 20 2.5" },
 			open: { d: "M 3 16.5 L 17 2.5" },
@@ -27,6 +28,7 @@ const ToggleButton = ({ setOpen }) => {
 		strokeWidth="3" 
 		stroke="black" 
 		strokeLinecap="round"
+		d= "M 3 2.5 L 17 16.4"
 		variants={{
 			closed: { d: "M 2 16.4 L 20 16.4" },
 			open: { d: "M 3 2.5 L 17 16.4" },
